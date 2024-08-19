fb1d2f2dd0f45f5f992444b1c31a86baf835d8c4
RapboyGao <atgao74201@qq.com>
2024-08-19 15:56:36 +0800

    2024.08.19

--- Sources/AViewUI/I18n.swift
+++ Sources/AViewUI/I18n.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+/// 支持以下语言：
+/// de, en, es, fr, it, ja, ko, pt, ru, th, zh-Hans, zh-Hant
 enum I18n {
     static let view = NSLocalizedString("edit.button.short", bundle: .module, comment: "The text shown on the edit button")
     static let edit = NSLocalizedString("view.button.short", bundle: .module, comment: "The text shown on the view button")
