513c6275298713c8c658f6bcfeca02c03538e84d
RapboyGao <atgao74201@qq.com>
2024-08-19 15:53:37 +0800

    2024.08.19

--- Sources/AViewUI/SheetButton/AGSheetButton-Extensions.swift
+++ Sources/AViewUI/SheetButton/AGSheetButton-Extensions.swift
@@ -1,10 +1,3 @@
-//
-//  File.swift
-//
-//
-//  Created by 高效奕 on 2023/8/29.
-//
-
 import Foundation
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
--- Sources/AViewUI/SheetButton/ASheetButton.swift
+++ Sources/AViewUI/SheetButton/ASheetButton.swift
@@ -1,10 +1,3 @@
-//
-//  MyFullScreenButton.swift
-//  Yunchi
-//
-//  Created by 高效奕 on 2023/2/22.
-//
-
 import SwiftUI
 
 @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
