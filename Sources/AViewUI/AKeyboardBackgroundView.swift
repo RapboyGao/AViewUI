b31d17c6611f41d75eeb921d096184850747536b
RapboyGao <atgao74201@qq.com>
2024-09-01 20:14:30 +0800

    2024.09.01

--- Sources/AViewUI/AKeyboardBackgroundView.swift
+++ Sources/AViewUI/AKeyboardBackgroundView.swift
@@ -1,12 +1,25 @@
 import SwiftUI
 
+@available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 6.0, *)
+private class AOrientationObserver: ObservableObject {
+    @Published var screenWidth: CGFloat = UIScreen.main.bounds.width
+
+    init() {
+        NotificationCenter.default.addObserver(self, selector: #selector(orientationDidChange), name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+
+    @objc private func orientationDidChange() {
+        screenWidth = UIScreen.main.bounds.width
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
+    }
+}
+
 @available(iOS 14.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 public struct AKeyboardBackgroundView<KeyboardContent: View>: View {
-    // 使用 @State 来存储屏幕宽度
-    @State private var screenWidth: CGFloat = UIScreen.main.bounds.width
-
-    // 使用 @Environment 获取当前的设备方向
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @StateObject private var orientation: AOrientationObserver = .init()
 
     private var makeContent: (CGFloat) -> KeyboardContent
     @Environment(\.colorScheme) private var colorScheme
@@ -23,14 +36,8 @@ public struct AKeyboardBackgroundView<KeyboardContent: View>: View {
     }
 
     public var body: some View {
-        makeContent(screenWidth)
+        makeContent(orientation.screenWidth)
             .background(boardColor())
-            .onChange(of: horizontalSizeClass) { _ in
-                DispatchQueue.main.async {
-                    // 当设备方向变化时更新屏幕宽度
-                    self.screenWidth = UIScreen.main.bounds.width
-                }
-            }
     }
 
     public init(@ViewBuilder makeContent: @escaping (CGFloat) -> KeyboardContent) {
