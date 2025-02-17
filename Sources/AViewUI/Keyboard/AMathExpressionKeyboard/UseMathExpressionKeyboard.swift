3ae3b7af58a28790f849d5767b273c3cb7bf39d5
RapboyGao <atgao74201@qq.com>
2025-02-17 15:27:28 +0800

    2025.02.17 为TextField扩展新增useAMathKeyboard方法，支持自定义高度和格式，优化数学表达式输入体验

--- Sources/AViewUI/Keyboard/AMathExpressionKeyboard/UseMathExpressionKeyboard.swift
+++ Sources/AViewUI/Keyboard/AMathExpressionKeyboard/UseMathExpressionKeyboard.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import AMathExpression
 
 @available(iOS 16.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
 extension TextField {
@@ -30,6 +31,30 @@ extension TextField {
         #if os(iOS)
         self.aKeyboardView { uiTextfield in
             AMathExpressionKeyboard(uiTextfield, bindString, format: format)
+                .frame(height: height)
+        }
+        #else
+        self.keyboardType(.decimalPad)
+        #endif
+    }
+    
+    @ViewBuilder
+    public func useAMathKeyboard<ANumber: Codable & Sendable & Real & BinaryFloatingPoint>(height: CGFloat, format: AMathFormatStyle<ANumber>) -> some View {
+        #if os(iOS)
+        self.aKeyboardView { uiTextfield in
+            AMathExpressionKeyboard(uiTextfield, format: format.displayedFormat)
+                .frame(height: height)
+        }
+        #else
+        self.keyboardType(.decimalPad)
+        #endif
+    }
+
+    @ViewBuilder
+    public func useAMathKeyboard<ANumber: Codable & Sendable & Real & BinaryFloatingPoint>(height: CGFloat, format: AMathFormatStyle<ANumber>, _ bindString: Binding<String>) -> some View {
+        #if os(iOS)
+        self.aKeyboardView { uiTextfield in
+            AMathExpressionKeyboard(uiTextfield, bindString, format: format.displayedFormat)
                 .frame(height: height)
         }
         #else
