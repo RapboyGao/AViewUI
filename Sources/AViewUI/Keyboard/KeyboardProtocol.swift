d0c396742d62237d51118093e9b9fb1539c8e9a6
RapboyGao <atgao74201@qq.com>
2026-02-04 11:02:16 +0800

    feat(键盘): 添加通用键盘协议及基础按键构建函数

---
+++ Sources/AViewUI/Keyboard/KeyboardProtocol.swift
@@ -0,0 +1,137 @@
+import SwiftUI
+
+#if os(iOS)
+
+/// 通用键盘协议：提供常用按键构建函数与基础样式。
+@available(iOS 15, *)
+public protocol KeyboardProtocol {
+    var input: ACustomKeyboardInputContext { get }
+    var lettersFont: Font { get }
+    var numbersFont: Font { get }
+    var keyCornerRadius: CGFloat { get }
+    var doneButtonTitle: String { get }
+}
+
+@available(iOS 15, *)
+public extension KeyboardProtocol {
+    var lettersFont: Font { .system(size: 10) }
+    var numbersFont: Font { .system(size: 23) }
+    var keyCornerRadius: CGFloat { 4 }
+    var doneButtonTitle: String { "Done" }
+}
+
+@available(iOS 16, *)
+public extension KeyboardProtocol where Self: View {
+    @ViewBuilder
+    func makeTextButton(_ text: String) -> some View {
+        AKeyButton(keyCornerRadius) {
+            input.insertText(text)
+        } content: { _ in
+            Text(text)
+                .font(numbersFont)
+        }
+    }
+
+    @ViewBuilder
+    func makeTextButton2(_ text: String) -> some View {
+        AKeyButton(keyCornerRadius, colors: .functionKeyColors) {
+            input.insertText(text)
+        } content: { _ in
+            Text(text)
+                .font(numbersFont)
+        }
+    }
+
+    @ViewBuilder
+    func makeNumberButton(_ number: Int) -> some View {
+        AKeyButton(keyCornerRadius) {
+            input.insertText(number.formatted(.number))
+        } content: { _ in
+            ANumKeyVStack(number, letters: lettersFont, number: numbersFont)
+        }
+    }
+
+    @ViewBuilder
+    func makeDeleteButton(beforeDelete: @escaping () -> Void = {}) -> some View {
+        AKeyButton(keyCornerRadius, colors: .functionKeyColors, sound: 1155) {
+            beforeDelete()
+            input.deleteBackward()
+        } content: { isPressed in
+            Image(systemName: isPressed ? "delete.left.fill" : "delete.left")
+                .font(.system(size: 24))
+                .fontWeight(.light)
+        }
+    }
+
+    @ViewBuilder
+    func makeDeleteButton2(beforeDelete: @escaping () -> Void = {}) -> some View {
+        AKeyButton(keyCornerRadius, sound: 1155) {
+            beforeDelete()
+            input.deleteBackward()
+        } content: { isPressed in
+            Image(systemName: isPressed ? "delete.left.fill" : "delete.left")
+                .font(.system(size: 24))
+                .fontWeight(.light)
+        }
+    }
+
+    @ViewBuilder
+    func equalButton(beforeEqual: @escaping () -> Void) -> some View {
+        AKeyButton(cornerRadius: keyCornerRadius) { isClicked, colorScheme in
+            if isClicked {
+                return AKeyColors.defaultColors.getColor(isClicked, colorScheme)
+            } else {
+                return .blue
+            }
+        } action: {
+            beforeDone()
+            input.dismissKeyboard()
+        } content: { isClicked in
+            Text("=")
+                .font(numbersFont)
+                .foregroundColor(isClicked ? .primary : .white)
+        }
+    }
+
+    @ViewBuilder
+    func doneButton(beforeDone: @escaping () -> Void) -> some View {
+        AKeyButton(cornerRadius: keyCornerRadius) { isClicked, colorScheme in
+            if isClicked {
+                return AKeyColors.defaultColors.getColor(isClicked, colorScheme)
+            } else {
+                return .blue
+            }
+        } action: {
+            beforeDone()
+            input.dismissKeyboard()
+        } content: { isClicked in
+            Text(doneButtonTitle)
+                .font(numbersFont)
+                .foregroundColor(isClicked ? .primary : .white)
+        }
+    }
+
+    @ViewBuilder
+    func clearButton(beforeClear: @escaping () -> Void = {}) -> some View {
+        AKeyButton(keyCornerRadius, colors: .functionKeyColors, sound: 1155) {
+            beforeClear()
+            input.clear()
+        } content: { _ in
+            Image(systemName: "arrow.counterclockwise")
+                .font(.system(size: 24))
+        }
+    }
+
+    @ViewBuilder
+    func clearButton2(beforeClear: @escaping () -> Void = {}) -> some View {
+        AKeyButton(keyCornerRadius, sound: 1155) {
+            beforeClear()
+            input.clear()
+        } content: { _ in
+            Image(systemName: "arrow.counterclockwise")
+                .font(.system(size: 24))
+        }
+    }
+}
+
+#endif
